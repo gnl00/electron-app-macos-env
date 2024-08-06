75567295f97c627ce0f680fdbf95fded10653f6c
gnl <gnl0@qq.com>
2024-08-06 23:25:28 +0800

    feat: add fetching state

--- src/renderer/src/pages/Home.tsx
+++ src/renderer/src/pages/Home.tsx
@@ -30,8 +30,7 @@ import {
 import { useEffect, useState } from 'react'
 import { PIN_WINDOW, GET_CONFIG, OPEN_EXTERNAL, SAVE_CONFIG } from '@constants/index'
 import { IAppConfig, ITranslateRequest } from '@types.d/index'
-import { translateRequest } from '@request/index'
-
+import { translateRequestWithHook } from '@request/index'
 // import { useEffectOnce } from 'react-use'
 
 const Home = (): JSX.Element => {
@@ -44,6 +43,7 @@ const Home = (): JSX.Element => {
     model: 'Qwen/Qwen2-7B-Instruct'
   })
   const [translateText, setTranslateText] = useState<string>('')
+  const [fetching, setFetchingState] = useState<boolean>(false)
 
   const { toast } = useToast()
 
@@ -102,7 +102,15 @@ const Home = (): JSX.Element => {
     setTranslateText(evt.target.value)
   }
 
-  const onSubmitClick = (): void => {
+  const startFetch = () => {
+    setFetchingState(true)
+  }
+
+  const endFetch = () => {
+    setFetchingState(false)
+  }
+
+  const onSubmitClick = async (): Promise<void> => {
     console.log('translateText', translateText)
     const req: ITranslateRequest = {
       url: appConfig.api,
@@ -111,14 +119,20 @@ const Home = (): JSX.Element => {
       prompt: appConfig.prompt,
       model: appConfig.model
     }
-    const result = translateRequest(req)
+
+    while (fetching) {
+      console.log('fetching...', new Date().getTime())
+    }
+
+    const result = await translateRequestWithHook(req, startFetch, endFetch)
+
     console.log('translate result: ', result);
   }
 
   return (
     <>
       <div className="m-2 app-dragable">
-        <div style={{}} className="flex justify-between w-full space-x-2">
+        <div className="flex justify-between w-full space-x-2">
           <Popover>
             <PopoverTrigger className="app-undragable">
               <div className="h-8 rounded-md px-3 border hover:bg-accent hover:text-accent-foreground flex items-center">
@@ -203,6 +217,15 @@ const Home = (): JSX.Element => {
           </Button>
         </div>
         <br></br>
+        {
+          fetching ? <>
+            <div className='flex'>
+              <Separator className='w-1/2' style={{ margin: '10px 0' }} />
+              <span className='text-xs text-slate-500'>translating...</span>
+              <Separator className='w-1/2' style={{ margin: '10px 0' }} />
+            </div>
+          </> : <></>
+        }
         <ScrollArea className="app-undragable h-96 min-h-1/3 w-full rounded-md border p-4">
           <Accordion type="multiple" defaultValue={['item-1', 'item-2', 'item-3']}>
             <AccordionItem value="item-1" data-state="open">
