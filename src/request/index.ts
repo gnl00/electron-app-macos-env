28660a2300b072e036e62e685a86f67127ebbd97
liguangning <liguangning@www.enmoli.com>
2024-08-06 18:30:59 +0800

    feat: send translate request

---
+++ src/request/index.ts
@@ -0,0 +1,57 @@
+import { IHeaders, ITranslateRequest } from '@types.d/index';
+
+const authorizationPreffix = 'Bearer '
+
+// const getHeanders: IHeaders = {}
+
+const postHeanders: IHeaders = {
+  'content-type': 'application/json'
+}
+
+export const translateRequest = async (req: ITranslateRequest) => {
+  if (!req.text) {
+    console.log('translate text is empty')
+    return
+  }
+
+  const initMessage = {
+    role: 'user',
+    content: req.prompt
+  }
+
+  const headers = {
+    ...postHeanders,
+    authorization: authorizationPreffix + req.token
+  }
+
+  console.log('request: ', req)
+  
+  const json = await fetch(req.url, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({
+      model: req.model,
+      messages: [
+        {...initMessage},
+        {
+          role: 'user',
+          content: req.text
+        }
+      ],
+      stream: false,
+      max_tokens: 512,
+      temperature: 0.7,
+      top_p: 0.7,
+      top_k: 50,
+      frequency_penalty: 0.5,
+      n: 1
+    })
+  }).then(resp => resp.json())
+  .catch(err => {
+    console.log('translateRequest ERROR', err)
+  })
+
+  console.log('json: ', json)
+
+  return json
+}
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,5 +1,20 @@
 export declare interface IAppConfig {
   token: string
-  api: string
+  api: string // default api
+  prompt: string, // default translate prompt
+  model: string // default model
+}
+
+export declare interface IHeaders {
+  accept?: string = 'application/json'
+  authorization?: string
+  'content-type'?: string
+}
+
+export declare interface ITranslateRequest {
+  url: string
+  token: string
+  text: string
+  model: string
   prompt: string
 }
