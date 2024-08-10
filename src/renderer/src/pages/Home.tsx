176968103d6876785882541923758cfba0d64728
gnl <gnl0@qq.com>
2024-08-10 14:51:29 +0800

    feat: language switch

--- src/renderer/src/pages/Home.tsx
+++ src/renderer/src/pages/Home.tsx
@@ -14,7 +14,8 @@ import {
   GearIcon,
   DrawingPinIcon,
   DrawingPinFilledIcon,
-  QuestionMarkCircledIcon
+  QuestionMarkCircledIcon,
+  SpaceEvenlyHorizontallyIcon
 } from '@radix-ui/react-icons'
 import { Input } from '@renderer/components/ui/input'
 import { Label } from '@renderer/components/ui/label'
@@ -27,11 +28,33 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@renderer/components/ui/tooltip"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@renderer/components/ui/select"
 import { useEffect, useRef, useState } from 'react'
 import { PIN_WINDOW, GET_CONFIG, OPEN_EXTERNAL, SAVE_CONFIG } from '@constants/index'
 import { translateRequestWithHook } from '@request/index'
 import ReactMarkdown from 'react-markdown'
 // import { useEffectOnce } from 'react-use'
+
+const languages = [
+  {
+    name: '中文',
+    value: 'zh'
+  },
+  {
+    name: '英语',
+    value: 'en'
+  },
+  {
+    name: '日语',
+    value: 'jp'
+  }
+]
 
 const Home = (): JSX.Element => {
 
@@ -42,10 +65,32 @@ const Home = (): JSX.Element => {
     prompt: '',
     model: 'Qwen/Qwen2-7B-Instruct'
   })
-  const [translateText, setTranslateText] = useState<string>('')
+  const [translateText, setTranslateText] = useState('')
   const [fetching, setFetchingState] = useState<boolean>(false)
-  const [translateResult, setTranslateResult] = useState<string>('')
-  const [defaultOpenValue, setDefaultOpenValue] = useState<string>('item-0')
+  const [translateResult, setTranslateResult] = useState('')
+  const [defaultOpenValue, setDefaultOpenValue] = useState('item-0')
+  const [sourceLanguage, setSourceLanguage] = useState('中文')
+  const [targetLanguage, setTargetLanguage] = useState('英文')
+  const [languagesChoise, setLanguagesChoise] = useState<Array<any>>([
+    {
+      id: 1,
+      name: '中文',
+      value: 'zh',
+      disable: false
+    },
+    {
+      id: 2,
+      name: '英文',
+      value: 'en',
+      disable: false
+    },
+    {
+      id: 3,
+      name: '日文',
+      value: 'jp',
+      disable: false
+    }
+  ])
   const scrollAreaEndRef = useRef<HTMLDivElement>(null)
 
   const { toast } = useToast()
@@ -122,12 +167,17 @@ const Home = (): JSX.Element => {
     // set result empty first
     setTranslateResult('')
 
+    const rawPrompt: string = appConfig.prompt
+    const prompt = rawPrompt.replace(/{{sourceLang}}/g, sourceLanguage).replace(/{{targetLang}}/g, targetLanguage)
+    
     const req: ITranslateRequest = {
       url: appConfig.api,
       text: translateText,
       token: appConfig.token,
-      prompt: appConfig.prompt,
-      model: appConfig?.model
+      prompt: prompt,
+      model: appConfig?.model,
+      sourceLang: sourceLanguage,
+      targetLang: targetLanguage
     }
 
     const reader = await translateRequestWithHook(req, beforeFetch, afterFetch)
@@ -173,6 +223,14 @@ const Home = (): JSX.Element => {
     setDefaultOpenValue('item-0')
   }
 
+  const onSourceLangSelected = (lang: string) => {
+    setSourceLanguage(lang)
+  }
+
+  const onTargetLangSelected = (lang: string) => {
+    setTargetLanguage(lang)
+  }
+
   return (
     <>
       <div className="m-2 app-dragable">
@@ -214,7 +272,7 @@ const Home = (): JSX.Element => {
                                 <QuestionMarkCircledIcon></QuestionMarkCircledIcon>
                               </TooltipTrigger>
                               <TooltipContent>
-                                <p>Models from <strong className='underline' onClick={(_) => onTokenQuestionClick('https://docs.siliconflow.cn/docs/model-names')}>SiliconFlow</strong></p>
+                                <p>Get <strong className='underline' onClick={(_) => onTokenQuestionClick('https://docs.siliconflow.cn/docs/model-names')}>SiliconFlow-Models</strong></p>
                               </TooltipContent>
                             </Tooltip>
                           </TooltipProvider>
@@ -242,7 +300,7 @@ const Home = (): JSX.Element => {
                                 <QuestionMarkCircledIcon></QuestionMarkCircledIcon>
                               </TooltipTrigger>
                               <TooltipContent>
-                                <p>Get token from <strong className='underline' onClick={(_) => onTokenQuestionClick('https://cloud.siliconflow.cn/account/ak')}>SiliconFlow</strong></p>
+                                <p>Get <strong className='underline' onClick={(_) => onTokenQuestionClick('https://cloud.siliconflow.cn/account/ak')}>SiliconFlow-Token</strong></p>
                               </TooltipContent>
                             </Tooltip>
                           </TooltipProvider>
@@ -277,6 +335,41 @@ const Home = (): JSX.Element => {
               </div>
             </PopoverContent>
           </Popover>
+          <div className='app-undragable flex space-x-2'>
+          <Select onValueChange={onSourceLangSelected} defaultValue={sourceLanguage}>
+            <SelectTrigger className="w-auto h-auto">
+              <SelectValue placeholder="sourceLang" />
+            </SelectTrigger>
+            <SelectContent>
+              {
+                languagesChoise.map((item, index) => {
+                  return (
+                    <>
+                      <SelectItem key={item.id} value={item.name}>{item.name}</SelectItem>
+                    </>
+                  )
+                })
+              }
+            </SelectContent>
+          </Select>
+          <Button className='h-auto w-auto' variant='secondary' size='sm'><SpaceEvenlyHorizontallyIcon /></Button>
+          <Select onValueChange={onTargetLangSelected} defaultValue={targetLanguage}>
+            <SelectTrigger className="w-auto h-auto">
+              <SelectValue placeholder="targetLang" />
+            </SelectTrigger>
+            <SelectContent>
+              {
+                languagesChoise.map((item, index) => {
+                  return (
+                    <>
+                      <SelectItem key={btoa(index + '')} value={item.name}>{item.name}</SelectItem>
+                    </>
+                  )
+                })
+              }
+            </SelectContent>
+          </Select>
+          </div>
           <Toggle className="app-undragable" size="xs" variant="outline" onClick={onPinToggleClick}>
             {pinState ? <DrawingPinFilledIcon /> : <DrawingPinIcon />}
           </Toggle>
